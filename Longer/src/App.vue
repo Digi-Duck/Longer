971dc5260e384e096dce0bd0c8694ca50c9b0089
ElenaWu171 <elenawu171@gmail.com>
2024-01-16 13:07:01 +0800

    初始版本

--- Longer/src/App.vue
+++ Longer/src/App.vue
@@ -1,25 +1,30 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "./components/HelloWorld.vue";
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <!-- <img
+      alt="Vue logo"
+      class="logo"
+      src="@/assets/logo.svg"
+      width="125"
+      height="125"
+    /> -->
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <!-- <div class="wrapper">
+      <HelloWorld msg="You did it!" /> -->
 
-      <nav>
-        <RouterLink to="/" class="text-[red]">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
+    <nav>
+      <RouterLink to="/" class="text-[red]">Home</RouterLink>
+    </nav>
+    <!-- </div> -->
   </header>
 
   <RouterView />
 </template>
-
+<!-- 
 <style scoped>
 header {
   line-height: 1.5;
@@ -82,4 +87,4 @@ nav a:first-of-type {
     margin-top: 1rem;
   }
 }
-</style>
+</style> -->
