eb55f2fbf0c0d05faea243611f29668bd332b90e
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-16 17:02:23 +0800

    0116: add nav style

--- Longer/src/App.vue
+++ Longer/src/App.vue
@@ -1,21 +1,5 @@
 <script>
 import { RouterLink, RouterView } from "vue-router";
-<<<<<<< Updated upstream
-// import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
-<template>
-  <header>
-    <!-- <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    /> -->
-    <!-- <div class="wrapper">
-      <HelloWorld msg="You did it!" /> -->
-=======
 import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
@@ -24,8 +8,8 @@ export default {
     RouterLink,
     RouterView
   },
-  data(){
-    return{
+  data() {
+    return {
       HelloWorld,
 
     }
@@ -35,30 +19,29 @@ export default {
 </script>
 
 <template>
-  <header class="w-[100vw] flex bg-[#ffa] p-0">
-    <RouterLink to="/" class="text-[red]">
-      <img src="./assets/img/generic/logoTest.PNG" alt="LOGO" class="w-[200px] inline">
+  <header class="">
+    <RouterLink to="/" class="navBar">
+      <img src="./assets/img/generic/logoTest.png" alt="LO GO">
     </RouterLink>
 
->>>>>>> Stashed changes
 
     <nav>
-      <RouterLink to="/about" class="text-[red]">
+      <RouterLink to="/about" class="navBar">
         關於我們
       </RouterLink>
-      <RouterLink to="/teacher" class="text-[red]">
+      <RouterLink to="/teacher" class="navBar">
         師資介紹
       </RouterLink>
-      <RouterLink to="/courseInformation" class="text-[red]">
+      <RouterLink to="/courseInformation" class="navBar">
         課程資訊
       </RouterLink>
-      <RouterLink to="/studentWork" class="text-[red]">
+      <RouterLink to="/studentWork" class="navBar">
         學生作品
       </RouterLink>
-      <RouterLink to="/admissionList" class="text-[red]">
+      <RouterLink to="/admissionList" class="navBar">
         歷年榜單
       </RouterLink>
-      <RouterLink to="/connection" class="text-[red]">
+      <RouterLink to="/connection" class="navBar">
         聯絡資訊
       </RouterLink>
     </nav>
@@ -71,65 +54,15 @@ export default {
   </footer>
 </template>
 
-<!-- <style scoped>
+<style lang="scss" scoped>
 header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
+  @apply flex justify-between items-end w-[100vw] p-0 bg-[#ffa];
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
+  .navBar {
+    @apply  mx-[5px] text-[1.8rem] text-[#fff] bg-[#050];
+    img{
+      @apply w-[300px];
+    }
   }
 }
-</style> -->
+</style>
