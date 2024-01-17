588e3ae24eccc2525238b97286c41aa458088606
Ray <104476691+Ray90000@users.noreply.github.com>
2024-01-17 16:06:27 +0800

    0117:studentComponent

--- Longer/src/views/StudentWork.vue
+++ Longer/src/views/StudentWork.vue
@@ -3,21 +3,20 @@ import StudentWork from "@/components/homepage/StudentWork.vue";
 export default {
 
     // import TheWelcome from '../components/TheWelcome.vue'
-        components: { StudentWork },
-        data(){
-            return {
-                studentData:[1,2,3,4,5],
-            }
-        },
+    components: { StudentWork },
+    data() {
+        return {
+            studentData: [1, 2, 3, 4, 5],
+        }
+    },
 }
 </script>
 <template lang="">
-    <div class="text-[80px]">
+    <div class="text-[80px] ">
         學生作品
     </div>
-    <div class="flex gap-2 border-[2px]">
-        <StudentWork v-for="item in studentData" :key="item.id" :student-data="item"></StudentWork>
-    </div>  
+        <StudentWork></StudentWork>
+
 </template>
 <style lang="">
     
