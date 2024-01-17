7d6d17302b562fa193b5d00f44c9277319002074
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-18 00:52:33 +0800

    0117: 完成banner + 畫室介紹

--- Longer/src/views/ConnectionUs.vue
+++ Longer/src/views/ConnectionUs.vue
@@ -15,15 +15,33 @@ export default {
     <section class="banner">
 
     </section>
+    <HomeTitle class="pl-[13px]">畫室介紹</HomeTitle>
     <section class="introduce">
-        <HomeTitle class="pl-[13px]">畫室介紹</HomeTitle>
+        <section class="introduce-text">
+            <p class="title">龍格畫室</p>
+            <p>一間成立於1994年的小畫室</p>
+            <p>位在三民路的小巷裡，由丁建中老師從零開始默默耕耘</p>
+            <p>至今已成為在地人口耳相傳的老字號畫室。</p>
+        </section>
     </section>
 </template>
 <style lang="scss" scoped>
     .banner{
-        @apply w-[100%] h-[calc(100vh_-_250px)] bg-gradient-to-l from-[url('../assets/img/generic/banner.png')] to-[blue];
+        @apply w-[100%] h-[calc(100vh_-_160px)] bg-[url('../assets/img/generic/banner.png')] bg-cover bg-no-repeat;
     }
     .introduce{
-        @apply w-[100%] h-[calc(100vh_-_152px)] bg-[url('../assets/img/generic/banner.png')] bg-cover bg-no-repeat;
+        @apply flex justify-center items-end w-[100%] h-[calc(100vh_-_152px)] pl-[150px] bg-[url('../assets/img/generic/introduce.png')] bg-cover bg-no-repeat;
+        writing-mode: vertical-rl;
+        .title{
+            @apply text-[40px] relative top-[-50px];
+
+        }
+        .introduce-text{
+            @apply text-[24px];
+
+            p{
+                @apply m-[10px];
+            }
+        }
     }
 </style>
