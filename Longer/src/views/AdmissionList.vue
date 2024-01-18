61f6b7914bcb8652ceecf7e648845bb94ce2a2f9
ElenaWu171 <elenawu171@gmail.com>
2024-01-18 17:11:31 +0800

    0118 : 師資介紹，完成

--- Longer/src/views/AdmissionList.vue
+++ Longer/src/views/AdmissionList.vue
@@ -1,13 +1,7 @@
 <script>
-export default {
-
-}
+export default {};
 </script>
 <template lang="">
-    <div class="text-[80px]">
-        歷年榜單
-    </div>
+  <div class="text-[80px]">歷年榜單</div>
 </template>
-<style lang="">
-    
-</style>
+<style lang=""></style>
