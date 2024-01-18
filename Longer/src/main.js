b38dd66eab1e5791628a517fe282eae89e9474ed
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-18 19:45:32 +0800

    Update main.js

--- Longer/src/main.js
+++ Longer/src/main.js
@@ -5,13 +5,13 @@ import "@/assets/font.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import AOS from "aos";
-import "aos/dist/aos.css";
+// import AOS from "aos";
+// import "aos/dist/aos.css";
 
 const app = createApp(App);
 
-app.config.globalProperties.$AOS = AOS;
-AOS.init();
+// app.config.globalProperties.$AOS = AOS;
+// AOS.init();
 app.use(router);
 
 app.mount("#app");
