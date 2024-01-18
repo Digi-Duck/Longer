9bc0f48f917bae4a55534e5d0df62c2de2dd367a
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-18 20:37:26 +0800

    Update main.js

--- Longer/src/main.js
+++ Longer/src/main.js
@@ -5,11 +5,12 @@ import "@/assets/font.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+// 報錯註解
 // import AOS from "aos";
 // import "aos/dist/aos.css";
 
 const app = createApp(App);
-
+// 報錯註解
 // app.config.globalProperties.$AOS = AOS;
 // AOS.init();
 app.use(router);
