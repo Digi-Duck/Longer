e229561a43b8c1f7e5b6870392f79f0a3d88151a
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-20 11:37:11 +0800

    0120: 學生作品頁排版

--- Longer/src/views/StudentWork.vue
+++ Longer/src/views/StudentWork.vue
@@ -4,36 +4,45 @@ export default {
 
   data() {
     return {
-      
+      studentWorks:[
+      '素描',
+      '水彩',
+      '水墨',
+      '色鉛筆類',
+      '麥克筆類',
+      '炭筆靜物類',
+      '石膏素描類',
+      '建築模型類',
+      '兒童美術創意類',
+      ],
     };
   },
 };
 </script>
 <template lang="">
   <main>
+
+
     <section>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
-      <div class="box"></div>
+      <div class="box" v-for="item in studentWorks" :key="item.id">
+        <div class="mask">{{item}}</div>
+      </div>
     </section>
 
   </main>
 </template>
 <style lang="scss" scoped>
   main{
-    @apply w-full py-[30px] bg-MainColorBG;
+    @apply flex justify-center items-center w-full h-[1000px] pt-0 bg-MainColorBG;
 
     section{
-      @apply flex flex-wrap gap-[30px] max-w-[810px] mx-[auto];
-
+      @apply flex flex-wrap gap-[15px] max-w-[810px] mx-[auto];
       .box{
-        @apply w-[220px] h-[220px] bg-[#aaf];
+        @apply flex items-center w-[260px] h-[260px] bg-[#aaf];
+
+        .mask{
+          @apply w-full py-[5px] text-center text-[#fff] bg-[rgba(0,0,0,0.7)];
+        }
       }
     }
   }
