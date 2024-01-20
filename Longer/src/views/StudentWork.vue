ff2d4c43e04fa8a413e2b5a98ada3343edfd5434
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-20 18:24:36 +0800

    0120: 新增學生駔品圖片

--- Longer/src/views/StudentWork.vue
+++ Longer/src/views/StudentWork.vue
@@ -1,19 +1,75 @@
 <script>
+import waterColor01 from '@/assets/img/student_work/water_color/S__13918247_0.jpg';
 
 export default {
 
   data() {
     return {
-      studentWorks:[
-      '素描',
-      '水彩',
-      '水墨',
-      '色鉛筆類',
-      '麥克筆類',
-      '炭筆靜物類',
-      '石膏素描類',
-      '建築模型類',
-      '兒童美術創意類',
+      studentWorks: [
+        {
+          skill: '素描',
+          img: [
+            waterColor01,
+
+          ],
+
+        },
+        {
+          skill: '水彩',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '水墨',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '色鉛筆類',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '麥克筆類',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '炭筆靜物類',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '石膏素描類',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '建築模型類',
+          img: [
+
+          ],
+
+        },
+        {
+          skill: '兒童美術創意類',
+          img: [
+
+          ],
+
+        },
       ],
     };
   },
@@ -25,26 +81,26 @@ export default {
 
     <section>
       <div class="box" v-for="item in studentWorks" :key="item.id">
-        <div class="mask">{{item}}</div>
+        <div class="mask">{{item.skill}}</div>
       </div>
     </section>
 
   </main>
 </template>
 <style lang="scss" scoped>
-  main{
-    @apply flex justify-center items-center w-full h-[1000px] pt-0 bg-MainColorBG;
+main {
+  @apply flex justify-center items-center w-full h-[1000px] pt-0 bg-MainColorBG;
 
-    section{
-      @apply flex flex-wrap gap-[15px] max-w-[810px] mx-[auto];
-      .box{
-        @apply flex items-center w-[260px] h-[260px] bg-[#aaf];
+  section {
+    @apply flex flex-wrap gap-[15px] max-w-[810px] mx-[auto];
 
-        .mask{
-          @apply w-full py-[5px] text-center text-[#fff] bg-[rgba(0,0,0,0.7)];
-        }
+    .box {
+      @apply flex items-center w-[260px] h-[260px] bg-[#aaf];
+
+      .mask {
+        @apply w-full py-[5px] text-center text-[#fff] bg-[rgba(0,0,0,0.7)];
       }
     }
   }
-
+}
 </style>
