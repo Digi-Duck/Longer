d3cd466ae9a3514669d6b164a5f3016b79191f18
ShunPing1 <73477487+ShunPing1@users.noreply.github.com>
2024-01-21 16:23:31 +0800

    0121: 學生作品 update

--- Longer/src/views/StudentWork.vue
+++ Longer/src/views/StudentWork.vue
@@ -1,11 +1,39 @@
 <script>
 // 導入lightbox
 import lightbox from 'lightbox2';
-// 導入圖片
-// import sketch01 from '@/assets/img/student_work/sketch/sketch01.jpg';
-import waterColor01 from '@/assets/img/student_work/water_color/water_color_01.jpg';
-import waterColor02 from '@/assets/img/student_work/water_color/water_color_02.jpg';
-import waterColor03 from '@/assets/img/student_work/water_color/water_color_03.jpg';
+// 導入圖片-素描
+import sketch01 from '@/assets/img/student_work/sketch_img/sketch_01.jpg';
+import sketch02 from '@/assets/img/student_work/sketch_img/sketch_02.jpg';
+import sketch03 from '@/assets/img/student_work/sketch_img/sketch_03.jpg';
+import sketch04 from '@/assets/img/student_work/sketch_img/sketch_04.jpg';
+import sketch05 from '@/assets/img/student_work/sketch_img/sketch_05.jpg';
+import sketch06 from '@/assets/img/student_work/sketch_img/sketch_06.jpg';
+// 導入圖片-水彩類
+import waterColor01 from '@/assets/img/student_work/water_color_img/water_color_01.jpg';
+import waterColor02 from '@/assets/img/student_work/water_color_img/water_color_02.jpg';
+import waterColor03 from '@/assets/img/student_work/water_color_img/water_color_03.jpg';
+import waterColor04 from '@/assets/img/student_work/water_color_img/water_color_04.jpg';
+import waterColor05 from '@/assets/img/student_work/water_color_img/water_color_05.jpg';
+import waterColor06 from '@/assets/img/student_work/water_color_img/water_color_06.jpg';
+import waterColor07 from '@/assets/img/student_work/water_color_img/water_color_07.jpg';
+import waterColor08 from '@/assets/img/student_work/water_color_img/water_color_08.jpg';
+import waterColor09 from '@/assets/img/student_work/water_color_img/water_color_09.jpg';
+import waterColor10 from '@/assets/img/student_work/water_color_img/water_color_10.jpg';
+// 導入圖片-水墨
+import ink01 from '@/assets/img/student_work/ink_img/ink_01.jpg';
+import ink02 from '@/assets/img/student_work/ink_img/ink_02.jpg';
+import ink03 from '@/assets/img/student_work/ink_img/ink_03.jpg';
+import ink04 from '@/assets/img/student_work/ink_img/ink_04.jpg';
+import ink05 from '@/assets/img/student_work/ink_img/ink_05.jpg';
+import ink06 from '@/assets/img/student_work/ink_img/ink_06.jpg';
+import ink07 from '@/assets/img/student_work/ink_img/ink_07.jpg';
+// 導入圖片-色鉛筆
+
+// 導入圖片-炭筆
+
+// 導入圖片-建築模型
+
+// 導入圖片-兒童美術
 
 
 export default {
@@ -16,65 +44,92 @@ export default {
       studentWorks: [
         {
           skill: '素描',
+          dataText:'sketch',
           img: [
-            // sketch01,
+            sketch01,
+            sketch02,
+            sketch03,
+            sketch04,
+            sketch05,
+            sketch06,
           ],
 
         },
         {
           skill: '水彩',
+          dataText:'waterColor',
           img: [
             waterColor01,
             waterColor02,
             waterColor03,
+            waterColor04,
+            waterColor05,
+            waterColor06,
+            waterColor07,
+            waterColor08,
+            waterColor09,
+            waterColor10,
 
           ],
 
         },
         {
           skill: '水墨',
+          dataText:'ink',
+          img: [
+            ink01,
+            ink02,
+            ink03,
+            ink04,
+            ink05,
+            ink06,
+            ink07,
+          ],
+
+        },
+        {
+          skill: '色鉛筆',
+          dataText:'waterColor',
           img: [
 
           ],
 
         },
         {
-          skill: '色鉛筆類',
+          skill: '麥克筆',
+          dataText:'waterColor',
           img: [
 
           ],
 
         },
         {
-          skill: '麥克筆類',
+          skill: '炭筆靜物',
+          dataText:'waterColor',
           img: [
 
           ],
 
         },
         {
-          skill: '炭筆靜物類',
+          skill: '石膏素描',
+          dataText:'waterColor',
           img: [
 
           ],
 
         },
         {
-          skill: '石膏素描類',
+          skill: '建築模型',
+          dataText:'waterColor',
           img: [
 
           ],
 
         },
         {
-          skill: '建築模型類',
-          img: [
-
-          ],
-
-        },
-        {
-          skill: '兒童美術創意類',
+          skill: '兒童美術創意',
+          dataText:'waterColor',
           img: [
 
           ],
@@ -93,10 +148,10 @@ export default {
 </script>
 <template lang="">
   <main>
-  
+  <img src="../assets/img/student_work/" alt="">
     <section>
       <div class="box" v-for="item in studentWorks" :key="item.id">
-        <a class="image-link" v-for="imgItem in item.img" :key="imgItem.id" :href="imgItem" data-lightbox="example-2" data-title="Optional caption.">
+        <a class="image-link" v-for="imgItem in item.img" :key="imgItem.id" :href="imgItem" :data-lightbox="item.dataText" data-title="Optional caption.">
           <div class="image"  :style="{ backgroundImage: `url(${imgItem})` }"></div>
         </a>
       </div>
@@ -109,29 +164,29 @@ export default {
   </main>
 </template>
 <style lang="scss" scoped>
-main {
-  @apply flex justify-center items-center w-full h-[1000px] pt-0 bg-MainColorBG;
+  main {
+    @apply flex justify-center items-center w-full h-[1000px] pt-0 bg-MainColorBG;
 
-  section {
-    @apply flex flex-wrap gap-[15px] max-w-[810px] mx-[auto];
+    section {
+      @apply flex flex-wrap gap-[15px] max-w-[810px] mx-[auto];
 
-    .box {
-      @apply flex items-center w-[260px] h-[260px] bg-[#aaf];
+      .box {
+        @apply flex items-center w-[260px] h-[260px] bg-[#aaf];
 
-      .image-link{
+        .image-link{
 
-        &:not(:first-child){
-          @apply hidden;
+          &:not(:first-child){
+            @apply hidden;
+          }
+          .image{
+            @apply w-[260px] h-[260px] bg-no-repeat bg-cover;
+    
+          }
         }
-        .image{
-          @apply w-[260px] h-[260px] bg-no-repeat bg-cover;
-  
-        }
+
+
       }
-
-
     }
-  }
 
-}
+  }
 </style>
