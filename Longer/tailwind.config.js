77dc241709b2357a79c85cd438ad120f552c2f4d
ElenaWu171 <145953965+ElenaWu171@users.noreply.github.com>
2024-01-24 21:55:13 +0800

    0124 :  完成師資簡介小圖

--- Longer/tailwind.config.js
+++ Longer/tailwind.config.js
@@ -3,6 +3,13 @@ export default {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      sm: '375px',
+      md: '481 px',
+      lg: '1200px',
+      xl:'1920px',
+     
+    },
     colors: {
       MainColorBG: "#fff8dc",
       MainColor: "#024b06",
