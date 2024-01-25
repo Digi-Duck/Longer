9fcf3403fd11a2a74ec53f03e76bcb84124ba718
Chuan jui <ray90130@gmail.com>
2024-01-25 22:35:25 +0800

    0125:aboutTeacher-RWD

--- Longer/src/components/homepage/AboutTeacher.vue
+++ Longer/src/components/homepage/AboutTeacher.vue
@@ -40,28 +40,34 @@ export default {
 </script>
 <template>
   <div class="AboutTeacherBG">
-    <div class="about">
-      <div class="th th-ph ">
+    <div class="about-l ">
+      <div class="th-ph th-r">
         <img class="align-left slide-in" :src="teacherAbout[0]" alt="" />
       </div>
-      <div class="th th-text  pr-[255px]">
-        富有教學熱忱的丁老師，創立畫室的初衷，是希望幫助每個對夢想懷抱憧憬的孩子，成為他們在追尋理想的人生道路上，一座座指引前方的燈塔。
+      <div class="th-text lg:mb-0 mb-[40px]">
+        <div class="lg:w-[70%] w-[90%]">
+          富有教學熱忱的丁老師，創立畫室的初衷，是希望幫助每個對夢想懷抱憧憬的孩子，成為他們在追尋理想的人生道路上，一座座指引前方的燈塔。
+        </div>
       </div>
     </div>
-    <div class="about">
-      <div class="th th-text  pl-[255px]">
-        升學集訓班將會集中編排程度相近的學生，課堂中親自示範與解題，使學生共同練習，透過同儕良性切磋相互學習。
+    <div class="about-r ">
+      <div class="th-text th-r lg:mb-0 mb-[40px]">
+        <div class="lg:w-[70%] w-[90%] ">
+          升學集訓班將會集中編排程度相近的學生，課堂中親自示範與解題，使學生共同練習，透過同儕良性切磋相互學習。
+        </div>
       </div>
-      <div class="th th-ph">
+      <div class="th-ph">
         <img class="align-right slide-in" :src="teacherAbout[1]" alt="" />
       </div>
     </div>
-    <div class="about">
-      <div class="th th-ph">
+    <div class="about-l">
+      <div class="th-ph th-r">
         <img class="align-left slide-in" :src="teacherAbout[2]" alt="" />
       </div>
-      <div class="th th-text  pr-[255px]">
-        掌控同一時段的授課人數，顧及每位學生的進度，讓來到龍格的學生，都能擁有良好的教學品質。　
+      <div class="th-text">
+        <div class="lg:w-[70%] w-[90%]">
+          掌控同一時段的授課人數，顧及每位學生的進度，讓來到龍格的學生，都能擁有良好的教學品質。　
+        </div>
       </div>
     </div>
   </div>
@@ -71,19 +77,36 @@ export default {
 .AboutTeacherBG {
   @apply w-[100%] h-[1919px] bg-MainColorBG;
 
-  .about {
-    @apply flex;
-
-    .th {
-      @apply  flex justify-center items-center;
-    }
-
+  .about-r{
+    @apply flex lg:flex-row lg:gap-32 flex-col-reverse gap-6 items-center;
     .th-text {
-      @apply w-[40%] xl:text-[1.75rem] lg:text-[1.5rem] md:text-[1.125rem] text-[.875rem] text-[GenWanMin-L] tracking-[.2em];
+      @apply w-[45%] leading-10 flex  items-center xl:text-[1.75rem] lg:text-[1.5rem]  md:text-[1.125rem] text-[.875rem] text-[GenWanMin-L] tracking-[.2em];
     }
-
+    .th-r{
+      @apply flex lg:justify-end  justify-center;
+    }
     .th-ph {
-      @apply w-[60%];
+      @apply w-[55%];
+      .slide-in {
+        @apply opacity-0 duration-700 ease-in;
+      }
+
+      .active {
+        @apply opacity-100;
+      }
+    }
+  }
+
+  .about-l {
+    @apply flex lg:flex-row lg:gap-32 flex-col gap-6 items-center;
+    .th-text {
+      @apply w-[45%] leading-10 flex  items-center xl:text-[1.75rem] lg:text-[1.5rem] lg:justify-start md:text-[1.125rem] justify-center text-[.875rem] text-[GenWanMin-L] tracking-[.2em];
+    }
+    .th-r{
+      @apply flex lg:justify-end  justify-center;
+    }
+    .th-ph {
+      @apply w-[55%];
       .slide-in {
         @apply opacity-0 duration-700 ease-in;
       }
